cbc111d4b71e5aefc84b819352a2ef15883773b9
Relex <relex@skyplatanus.com>
2016-07-11 13:07:26 +0800

    判断 MIUI 显示改为 判断是否允许通知。 兼容更多设备。 MIUI 8 会禁用了 TYPE_TOAST 类型 , 只好用Try防止崩溃。

--- app/src/main/java/io/github/zhitaocai/toastcompat/demo/MainActivity.java
+++ app/src/main/java/io/github/zhitaocai/toastcompat/demo/MainActivity.java
@@ -10,90 +10,86 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
 import io.github.zhitaocai.toastcompat.IToast;
 import io.github.zhitaocai.toastcompat.ToastCompat;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-		findViewById(R.id.btn_show_one_toast).setOnClickListener(this);
-		findViewById(R.id.btn_show_multi_toast).setOnClickListener(this);
-		findViewById(R.id.btn_show_toast_test).setOnClickListener(this);
-		findViewById(R.id.btn_cancel_toast_test).setOnClickListener(this);
+        ToastCompat.checkToastEnable(getApplicationContext(), true);
 
-		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-		setSupportActionBar(toolbar);
+        findViewById(R.id.btn_show_one_toast).setOnClickListener(this);
+        findViewById(R.id.btn_show_multi_toast).setOnClickListener(this);
+        findViewById(R.id.btn_show_toast_test).setOnClickListener(this);
+        findViewById(R.id.btn_cancel_toast_test).setOnClickListener(this);
 
-		FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-		fab.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				Snackbar.make(view, "BugReports via czt.saisam@gmail.com", Snackbar.LENGTH_LONG).setAction("Action", null)
-						.show();
-			}
-		});
-	}
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-	private IToast mToast;
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View view) {
+                Snackbar.make(view, "BugReports via czt.saisam@gmail.com", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null)
+                        .show();
+            }
+        });
+    }
 
-	@Override
-	public void onClick(View v) {
-		if (v instanceof Button) {
-			switch (v.getId()) {
+    private IToast mToast;
 
-			case R.id.btn_show_one_toast:
-				ToastCompat.makeText(this, "show One Toast", Toast.LENGTH_SHORT).show();
-				break;
+    @Override public void onClick(View v) {
+        if (v instanceof Button) {
+            switch (v.getId()) {
 
-			case R.id.btn_show_multi_toast:
-				ToastCompat.makeText(this, "show Multi Toast : one", Toast.LENGTH_SHORT).show();
-				ToastCompat.makeText(this, "show Multi Toast : two", Toast.LENGTH_SHORT).show();
-				ToastCompat.makeText(this, "show Multi Toast : three", Toast.LENGTH_SHORT).show();
-				//				Toast.makeText(this, "show Multi Toast : one", Toast.LENGTH_SHORT).show();
-				//				Toast.makeText(this, "show Multi Toast : two", Toast.LENGTH_SHORT).show();
-				//				Toast.makeText(this, "show Multi Toast : three", Toast.LENGTH_SHORT).show();
-				break;
+                case R.id.btn_show_one_toast:
+                    ToastCompat.makeText(this, "show One Toast", Toast.LENGTH_SHORT).show();
+                    break;
 
-			case R.id.btn_show_toast_test:
-				mToast = new ToastCompat(this).setText("test").setDuration(Toast.LENGTH_SHORT);
-				mToast.show();
-				break;
+                case R.id.btn_show_multi_toast:
+                    ToastCompat.makeText(this, "show Multi Toast : one", Toast.LENGTH_SHORT).show();
+                    ToastCompat.makeText(this, "show Multi Toast : two", Toast.LENGTH_SHORT).show();
+                    ToastCompat.makeText(this, "show Multi Toast : three", Toast.LENGTH_SHORT)
+                            .show();
+                    //				Toast.makeText(this, "show Multi Toast : one", Toast.LENGTH_SHORT).show();
+                    //				Toast.makeText(this, "show Multi Toast : two", Toast.LENGTH_SHORT).show();
+                    //				Toast.makeText(this, "show Multi Toast : three", Toast.LENGTH_SHORT).show();
+                    break;
 
-			case R.id.btn_cancel_toast_test:
-				if (mToast != null) {
-					mToast.cancel();
-				}
-				break;
+                case R.id.btn_show_toast_test:
+                    mToast = new ToastCompat(this).setText("test").setDuration(Toast.LENGTH_SHORT);
+                    mToast.show();
+                    break;
 
-			}
-		}
-	}
+                case R.id.btn_cancel_toast_test:
+                    if (mToast != null) {
+                        mToast.cancel();
+                    }
+                    break;
+            }
+        }
+    }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.menu_main, menu);
-		return true;
-	}
+    @Override public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
+    @Override public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
 
-		//noinspection SimplifiableIfStatement
-		if (id == R.id.action_settings) {
-			return true;
-		}
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
 
-		return super.onOptionsItemSelected(item);
-	}
-
+        return super.onOptionsItemSelected(item);
+    }
 }
--- toastcompat/src/main/java/io/github/zhitaocai/toastcompat/ToastCompat.java
+++ toastcompat/src/main/java/io/github/zhitaocai/toastcompat/ToastCompat.java
@@ -1,11 +1,11 @@
 package io.github.zhitaocai.toastcompat;
 
 import android.content.Context;
+import android.os.Build;
 import android.view.Gravity;
 import android.view.View;
-
 import io.github.zhitaocai.toastcompat.util.DisplayUtil;
-import io.github.zhitaocai.toastcompat.util.OSJudgementUtil;
+import io.github.zhitaocai.toastcompat.util.NotificationManagerCompatKitKat;
 
 /**
  * @author zhitao
@@ -13,67 +13,76 @@ import io.github.zhitaocai.toastcompat.util.OSJudgementUtil;
  */
 public class ToastCompat implements IToast {
 
-	private IToast mIToast;
+    private static boolean sIsJudgementBefore = false;
 
-	public ToastCompat(Context context) {
-		this(context, null, -1);
-	}
+    private static boolean sIsToastEnable = false;
 
-	ToastCompat(Context context, String text, int duration) {
-		if (OSJudgementUtil.isMIUI()) {
-			mIToast = new MIUIToast(context).setText(text).setDuration(duration)
-					.setGravity(Gravity.BOTTOM, 0, DisplayUtil.dip2px(context, 64));
-		} else {
-			mIToast = new SystemToast(context).setText(text).setDuration(duration);
-		}
-	}
+    private IToast mIToast;
 
-	public static IToast makeText(Context context, String text, int duration) {
-		return new ToastCompat(context, text, duration);
-	}
+    public ToastCompat(Context context) {
+        this(context, null, -1);
+    }
 
-	@Override
-	public IToast setGravity(int gravity, int xOffset, int yOffset) {
-		return mIToast.setGravity(gravity, xOffset, yOffset);
-	}
+    public static boolean checkToastEnable(Context context, boolean forceCheck) {
+        if (!sIsJudgementBefore || forceCheck) {
+            sIsJudgementBefore = true;
+            sIsToastEnable = areNotificationsEnabled(context.getApplicationContext());
+        }
+        return sIsToastEnable;
+    }
 
-	@Override
-	public IToast setDuration(long durationMillis) {
-		return mIToast.setDuration(durationMillis);
-	}
+    public static boolean areNotificationsEnabled(Context context) {
+        if (Build.VERSION.SDK_INT >= 19) {
+            return NotificationManagerCompatKitKat.areNotificationsEnabled(context);
+        }
+        return true;
+    }
 
-	/**
-	 * 不能和{@link #setText(String)}一起使用，要么{@link #setView(View)} 要么{@link #setView(View)}
-	 *
-	 * @param view
-	 */
-	@Override
-	public IToast setView(View view) {
-		return mIToast.setView(view);
-	}
+    ToastCompat(Context context, String text, int duration) {
+        if (checkToastEnable(context, false)) {
+            mIToast = new SystemToast(context).setText(text).setDuration(duration);
+        } else {
+            mIToast = new CustomToast(context).setText(text)
+                    .setDuration(duration)
+                    .setGravity(Gravity.BOTTOM, 0, DisplayUtil.dip2px(context, 64));
+        }
+    }
 
-	@Override
-	public IToast setMargin(float horizontalMargin, float verticalMargin) {
-		return mIToast.setMargin(horizontalMargin, verticalMargin);
-	}
+    public static IToast makeText(Context context, String text, int duration) {
+        return new ToastCompat(context, text, duration);
+    }
 
-	/**
-	 * 不能和{@link #setView(View)}一起使用，要么{@link #setView(View)} 要么{@link #setView(View)}
-	 *
-	 * @param text
-	 */
-	@Override
-	public IToast setText(String text) {
-		return mIToast.setText(text);
-	}
+    @Override public IToast setGravity(int gravity, int xOffset, int yOffset) {
+        return mIToast.setGravity(gravity, xOffset, yOffset);
+    }
 
-	@Override
-	public void show() {
-		mIToast.show();
-	}
+    @Override public IToast setDuration(long durationMillis) {
+        return mIToast.setDuration(durationMillis);
+    }
 
-	@Override
-	public void cancel() {
-		mIToast.cancel();
-	}
+    /**
+     * 不能和{@link #setText(String)}一起使用，要么{@link #setView(View)} 要么{@link #setView(View)}
+     */
+    @Override public IToast setView(View view) {
+        return mIToast.setView(view);
+    }
+
+    @Override public IToast setMargin(float horizontalMargin, float verticalMargin) {
+        return mIToast.setMargin(horizontalMargin, verticalMargin);
+    }
+
+    /**
+     * 不能和{@link #setView(View)}一起使用，要么{@link #setView(View)} 要么{@link #setView(View)}
+     */
+    @Override public IToast setText(String text) {
+        return mIToast.setText(text);
+    }
+
+    @Override public void show() {
+        mIToast.show();
+    }
+
+    @Override public void cancel() {
+        mIToast.cancel();
+    }
 }
